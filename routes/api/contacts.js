88fef959d41a9c72180bcd2a757c01b517b3edbb
medeea98 <142827871+medeea98@users.noreply.github.com>
2024-07-18 12:16:17 +0300

    first commit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,112 +1,38 @@
 const express = require("express");
+
 const {
   listContacts,
   getContactById,
   addContact,
   removeContact,
   updateContact,
+  updateStatusContact,
 } = require("../../models/contacts");
-const Joi = require("joi");
 
 const router = express.Router();
 
-const itemsSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string()
-    .regex(/^[0-9]{10}$/)
-    .messages({
-      "string.pattern.base": `Phone number must have 10 digits and only numbers.`,
-    })
-    .required(),
+router.get("/", (req, res) => {
+  listContacts(res);
 });
 
-router.get("/", async (req, res, next) => {
-  const contacts = await listContacts();
-  res.status(200).json({
-    data: {
-      contacts,
-    },
-  });
+router.get("/:contactId", (req, res) => {
+  getContactById(res, req.params.contactId);
 });
 
-router.get("/:contactId", async (req, res, next) => {
-  const contact = await getContactById(req.params.contactId);
-
-  if (!contact) {
-    res.status(404).json({
-      message: "Not Found",
-    });
-  } else {
-    res.status(200).json({
-      contact,
-    });
-  }
+router.post("/", (req, res) => {
+  addContact(res, req.body);
 });
 
-router.post("/", async (req, res, next) => {
-  const { error } = itemsSchema.validate(req.body);
-
-  if (error) {
-    return res.status(400).json({
-      message: error.message,
-    });
-  }
-
-  const newContact = await addContact(req.body);
-  res.status(201).json({
-    id: {
-      id: newContact.id,
-      name: newContact.name,
-      email: newContact.email,
-      phone: newContact.phone,
-    },
-  });
+router.delete("/:contactId", (req, res) => {
+  removeContact(res, req.params.contactId);
 });
 
-router.delete("/:contactId", async (req, res, next) => {
-  const contact = await getContactById(req.params.contactId);
-
-  if (!contact) {
-    return res.status(404).json({
-      message: "Not Found",
-    });
-  }
-
-  await removeContact(req.params.contactId);
-  res.status(200).json({
-    message: "contact deleted",
-  });
+router.put("/:contactId", (req, res) => {
+  updateContact(res, req.body, req.params.contactId);
 });
 
-router.put("/:contactId", async (req, res, next) => {
-  const contact = await getContactById(req.params.contactId);
-
-  if (!contact) {
-    return res.status(404).json({
-      message: "Not Found",
-    });
-  }
-
-  if (Object.keys(req.body).length === 0) {
-    return res.status(400).json({
-      message: "missing fields",
-    });
-  }
-
-  const { error } = itemsSchema.validate(req.body);
-
-  if (error) {
-    return res.status(400).json({
-      message: error.message,
-    });
-  }
-
-  const response = await updateContact(req.params.contactId, req.body);
-
-  res.status(200).json({
-    contact: response,
-  });
+router.patch("/:contactId/favorite", (req, res) => {
+  updateStatusContact(res, req.body, req.params.contactId);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+
+const URL =
+  "mongodb+srv://medeea:parola@cluster0.rntxdlp.mongodb.net/db-contacts";
+
+mongoose
+  .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Database connection succesful"))
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
 
 app.listen(3000, () => {
   console.log("Server is running. Use our API on port: 3000");
