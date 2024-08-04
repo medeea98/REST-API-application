4ada06d2fc9715c5b6d8c5aa1f69de97e302eec7
medeea98 <142827871+medeea98@users.noreply.github.com>
2024-08-04 16:45:19 +0300

    hw05

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const path = require("path");
 
 const contactsRouter = require("./routes/api/contacts");
 const usersRouter = require("./routes/api/users");
@@ -13,6 +14,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/avatars", express.static(path.join(__dirname, "public", "avatars")));
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", usersRouter);
 
